c5e91b58b9944348ceb3c119ba4c66c9c22b3ce4
Chiminka <58224752+Chiminka@users.noreply.github.com>
2022-10-14 22:46:17 +0200

    Delete models directory

--- models/Category.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const CategorySchema = new mongoose.Schema(
-    {
-        title: { type: String, required: true, default: '' },
-        description: { type: String, required: true, default: '' },
-    }
-)
-export default mongoose.model('Category', CategorySchema)
